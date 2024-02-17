8c1a87027e30e1d722a839c9c14e55d99c27ee49
IamSomewrong <gonka04@gmail.com>
2024-02-17 09:59:39 +0300

    Готовы сохранения и выгрузка на платформах

--- DiplomAttempt2/PackageContentPage.xaml.cs
+++ DiplomAttempt2/PackageContentPage.xaml.cs
@@ -15,21 +15,7 @@ public partial class PackageContentPage : ContentPage
     {
         var name = await DisplayPromptAsync("Новый пакет", "Введите название:");
         App.Packages.Packages.Add(new Package() { Name = name });
-#if ANDROID
-        string str = JsonSerializer.Serialize<SerializablePackages>(App.Packages);
-        FileStream fs = new(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal) + "/packs.json", FileMode.Create);
-        StreamWriter sw = new StreamWriter(fs, System.Text.Encoding.UTF8);
-        sw.Write(str);
-        sw.Close();
-        fs.Close();
-#elif WINDOWS
-        string str = JsonSerializer.Serialize<SerializablePackages>(App.Packages);
-        string path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\packs.json";
-        FileStream fs = new(path, FileMode.Create);
-        StreamWriter sw = new StreamWriter(fs, System.Text.Encoding.UTF8);
-        sw.Write(str);
-        sw.Close();
-        fs.Close();
-#endif
+        var writeData = JsonSerializer.Serialize(App.Packages);
+        File.WriteAllText(App._fileName, writeData);
     }
 }
