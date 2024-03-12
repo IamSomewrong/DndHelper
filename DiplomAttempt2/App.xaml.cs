801ab98ba425671fff57a3fc42908f091f3bcceb
IamSomewrong <gonka04@gmail.com>
2024-03-12 19:13:32 +0300

    Переписалл сохранения и загрузку. Теперь пакет - файл.

--- DiplomAttempt2/App.xaml.cs
+++ DiplomAttempt2/App.xaml.cs
@@ -1,24 +1,40 @@
 ﻿using DiplomAttempt2.Models;
 using System.Collections.ObjectModel;
 using System.Text.Json;
-
 namespace DiplomAttempt2;
 
 public partial class App : Application
 {
-	public static SerializablePackages Packages;
-    public static string _fileName = FileSystem.AppDataDirectory + "/packs.json";
+	public static ObservableCollection<Package> Packages;
+    public static string[] _files = Directory.GetFiles(FileSystem.AppDataDirectory, "*.pckg");
     public App()
 	{
 		InitializeComponent();
-        Packages = new SerializablePackages() { Packages = new ObservableCollection<Package>() };
-        if (File.Exists(_fileName) == false)
+        Packages = new ObservableCollection<Package>();
+        if (_files.Length == 0)
         {
-            var writeData = JsonSerializer.Serialize(Packages);
-            File.WriteAllText(_fileName, writeData);
+            Package package = new Package()
+            {
+                Name = "Базовый пакет",
+                Classes = new ObservableCollection<Class>(),
+                Races = new ObservableCollection<Race>(),
+                Items = new ObservableCollection<Item>(),
+                Enemies = new ObservableCollection<Enemy>(),
+                Spells = new ObservableCollection<Spell>(),
+            };
+            var writeData = JsonSerializer.Serialize(package);
+            File.WriteAllText(FileSystem.AppDataDirectory + "/base.pckg", writeData);
+            Packages.Add(package);
         }
-        var rawData = File.ReadAllText(_fileName);
-        Packages = JsonSerializer.Deserialize<SerializablePackages>(rawData);
+        else
+        {
+            foreach(var file in _files)
+            {
+                var rawData = File.ReadAllText(file);
+                Package package = JsonSerializer.Deserialize<Package>(rawData);
+                Packages.Add(package);
+            }
+        }
 
         MainPage = new AppShell();
 	}
--- DiplomAttempt2/PackageContentPage.xaml.cs
+++ DiplomAttempt2/PackageContentPage.xaml.cs
@@ -8,14 +8,31 @@ public partial class PackageContentPage : ContentPage
     public PackageContentPage()
 	{
 		InitializeComponent();
-        PackageView.ItemsSource = App.Packages.Packages;
+        PackageView.ItemsSource = App.Packages;
     }
 
     public async void CreatePackage(object sender, EventArgs e)
     {
         var name = await DisplayPromptAsync("Новый пакет", "Введите название:");
-        App.Packages.Packages.Add(new Package() { Name = name });
-        var writeData = JsonSerializer.Serialize(App.Packages);
-        File.WriteAllText(App._fileName, writeData);
+        if(name.Length > 0) 
+        {
+            Package package = new Package() { 
+                Name = name, 
+                Classes = new ObservableCollection<Class>(),
+                Races = new ObservableCollection<Race>(),
+                Items = new ObservableCollection<Item>(),
+                Enemies = new ObservableCollection<Enemy>(),
+                Spells = new ObservableCollection<Spell>(),
+            };
+            App.Packages.Add(package);
+            var writeData = JsonSerializer.Serialize(package);
+            File.WriteAllText(FileSystem.AppDataDirectory + "/" + name + ".pckg", writeData);
+        }
+        
+    }
+
+    public async void OpenPackage(object sender, ItemTappedEventArgs e)
+    {
+        await Navigation.PushAsync(new PackagePage(App.Packages[e.ItemIndex]));
     }
 }
